9491e3a1c90f7479caa13e0ac52ae7fbe0449c93
tienhoangg <hoangtrantien202@gmail.com>
2022-08-02 06:01:46 +0700

    happy

--- src/controllers/getRandView.js
+++ src/controllers/getRandView.js
@@ -2,6 +2,7 @@ const getRandView = require('../services/cateOfImage.service');
 exports.getview = async (req, res) => {
     const Tags = req.body.category;
     const listTag = Tags.split(',');
-    list = {}
-    res.json(list);
+    const num = req.body.num;
+    const result = await getRandView.getRandView(listTag, num);
+    res.json(result);
 }
