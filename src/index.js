3a8402fff8772b77f14f41ccfb406dd7cbe4e133
tienhoangg <hoangtrantien202@gmail.com>
2022-08-05 20:09:22 +0700

    update cors

--- src/index.js
+++ src/index.js
@@ -2,32 +2,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookie = require('cookie-parser');
 var app = express();
+app.use(cookie());
 const connectDB = require("./config/db-config");
 connectDB();
-// var cors = require('cors')
-// app.use(cors());
-app.use(cookie());
+var cors = require('cors')
+app.use(cors(
+    {
+        origin: 'https://wibuwallpaper.azurewebsites.net',
+        credentials: true,
+        exposedHeaders: ["set-cookie"],
+        allowedHeaders: ["Content-Type", "Authorization", "set-cookie"],
+        methods: ["GET", "POST", "PUT", "DELETE"]
+    }
+));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-// // app.use(function (req, res, next) {
-
-// //     // Website you wish to allow to connect
-// //     res.setHeader('Access-Control-Allow-Origin', '*');
-
-// //     // Request methods you wish to allow
-// //     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-// //     // Request headers you wish to allow
-// //     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-// //     // Set to true if you need the website to include cookies in the requests sent
-// //     // to the API (e.g. in case you use sessions)
-// //     res.setHeader('Access-Control-Allow-Credentials', true);
-
-// //     // Pass to next layer of middleware
-// //     next();
-// // });
 
 app.use('/users', require("./routes/users"));
 app.use('/feedback', require("./routes/feedback"));
